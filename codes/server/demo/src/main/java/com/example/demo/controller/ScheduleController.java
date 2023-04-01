3d8298a3131c60a10fe52a32d8b6687fee1f843a
geniuus <cross_man@naver.com>
2023-04-01 22:46:41 +0900

    ScheduleController 수정중

--- codes/server/demo/src/main/java/com/example/demo/controller/ScheduleController.java
+++ codes/server/demo/src/main/java/com/example/demo/controller/ScheduleController.java
@@ -1,9 +1,11 @@
 package com.example.demo.controller;
 
+import antlr.Token;
 import com.example.demo.dto.ResponseDTO;
 import com.example.demo.dto.ScheduleDTO;
 import com.example.demo.model.ScheduleEntity;
 import com.example.demo.model.TodoEntity;
+import com.example.demo.security.TokenProvider;
 import com.example.demo.service.ScheduleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -23,12 +25,18 @@ public class ScheduleController {
     @Autowired
     private ScheduleService service;
 
+	private TokenProvider tokenProvider;
+
     @PostMapping
 	public ResponseEntity<?> createSchedule(@AuthenticationPrincipal String userId, @RequestBody ScheduleDTO dto) {
 		try {
 			ScheduleEntity entity = ScheduleDTO.toEntity(dto);
 
-			entity.setId(null);
+			// dto에서 token 가져온 후에
+			String token = dto.getToken();
+
+			// tokenProvider로 토큰 해석해서 userId 알아냄
+			tokenProvider.validateAndGetUserId(token);
 
 			entity.setUserId(userId);
 
