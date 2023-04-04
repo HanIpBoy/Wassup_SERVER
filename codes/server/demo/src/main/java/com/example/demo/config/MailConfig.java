eeccd976b604ddb5e6a9d536d2d90339575351be
terry8408@gmail.com <96044823+terry8408@users.noreply.github.com>
2023-04-04 17:14:49 +0900

    MailConfig 수정
    MailConfig 수정(모로코 해킹 때문에 계정 삭제 후 새로 생성)

--- codes/server/demo/src/main/java/com/example/demo/config/MailConfig.java
+++ codes/server/demo/src/main/java/com/example/demo/config/MailConfig.java
@@ -15,8 +15,8 @@ public class MailConfig {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com"); // SMTP 서버 주소
         mailSender.setPort(587); // SMTP 포트
-        mailSender.setUsername("wassup.hansung@gmail.com"); // SMTP 계정 이메일
-        mailSender.setPassword("iwwodrjyrrhyxrvt"); // SMTP 계정 비밀번호
+        mailSender.setUsername("hansung.wassup@gmail.com"); // SMTP 계정 이메일
+        mailSender.setPassword("inhiorrhwspdlotc"); // SMTP 계정 비밀번호
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.smtp.auth", "true");
