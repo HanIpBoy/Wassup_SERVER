033f0529e27adfef80012109b93383f7e9989f60
terry8408@gmail.com <96044823+terry8408@users.noreply.github.com>
2023-04-14 18:22:45 +0900

    MailConfig 외부 환경 설정 파일로 암호화
    MailConfig 외부 환경 설정 파일로 암호화

--- codes/server/demo/src/main/java/com/example/demo/config/MailConfig.java
+++ codes/server/demo/src/main/java/com/example/demo/config/MailConfig.java
@@ -1,5 +1,6 @@
 package com.example.demo.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -9,14 +10,24 @@ import java.util.Properties;
 
 @Configuration
 public class MailConfig {
+    @Value("${mail.host}")
+    private String host;
 
+    @Value("${mail.port}")
+    private int port;
+
+    @Value("${mail.username}")
+    private String username;
+
+    @Value("${mail.password}")
+    private String password;
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com"); // SMTP 서버 주소
-        mailSender.setPort(587); // SMTP 포트
-        mailSender.setUsername("hansung.wassup@gmail.com"); // SMTP 계정 이메일
-        mailSender.setPassword("inhiorrhwspdlotc"); // SMTP 계정 비밀번호
+        mailSender.setHost(host); // SMTP 서버 주소
+        mailSender.setPort(port); // SMTP 포트
+        mailSender.setUsername(username); // SMTP 계정 이메일
+        mailSender.setPassword(password); // SMTP 계정 비밀번호
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.smtp.auth", "true");
