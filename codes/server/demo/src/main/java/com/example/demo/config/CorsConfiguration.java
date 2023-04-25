15172032cb59e4f40fed18f08f0d90e47ff3551b
terry8408@gmail.com <96044823+terry8408@users.noreply.github.com>
2023-04-25 15:56:30 +0900

    CorsConfiguration 수정 2
    CorsConfiguration 수정 2

--- codes/server/demo/src/main/java/com/example/demo/config/CorsConfiguration.java
+++ codes/server/demo/src/main/java/com/example/demo/config/CorsConfiguration.java
@@ -9,7 +9,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 요청에 대해
-                .allowedOrigins("localhost:3000") // 허용할 도메인
+                .allowedOrigins("http://localhost:3000") // 허용할 도메인
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 메소드
                 .allowedHeaders("*") // 허용할 헤더
                 .allowCredentials(true); // 쿠키를 허용할지 여부
