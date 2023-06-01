03b16552078d963ef9f19b9e26997ace2b22d8da
geniuus <cross_man@naver.com>
2023-06-02 05:35:58 +0900

    mail 부분 rollback, 빌드 포함

--- codes/server/demo/src/main/java/com/example/demo/service/MailService.java
+++ codes/server/demo/src/main/java/com/example/demo/service/MailService.java
@@ -23,38 +23,18 @@ public class MailService {
     @Autowired
     private UserRepository userRepository;
 
-//    public void send(UserEntity userEntity){
-//        SimpleMailMessage message = new SimpleMailMessage();
-//        message.setTo(userEntity.getUserId()); // To: 수신자
-//        message.setSubject("이때 모해? 이메일 계정 인증"); // Subject 제목
-//        message.setText("이때 모해? 계정 인증을 위해 아래의 인증 코드를 사용해주세요:\n\n인증 코드: " +userEntity.getEmailAuthCode()+" \n\n 계정 보안을 위해 인증 코드를 타인과 공유하지 마세요. 저희는 고객의 개인 정보를 중요하게 여기며, 인증 과정을 통해 안전한 서비스를 제공하고 있습니다.\n" +
-//                "\n" +
-//                "궁금한 사항이 있으시면 언제든지 문의해주세요.\n" +
-//                "\n" +
-//                "감사합니다.");
-//        mailSender.send(message);
-//    }
-
     public void send(UserEntity userEntity){
-        MimeMessage message = mailSender.createMimeMessage();
-
-        try {
-            MimeMessageHelper helper = new MimeMessageHelper(message, true);
-            helper.setTo(userEntity.getUserId());
-            helper.setSubject("이때 모해? 이메일 계정 인증");
-
-            String text = "이때 모해? 계정 인증을 위해 아래의 인증 코드를 사용해주세요:\n\n인증 코드: " + userEntity.getEmailAuthCode() + "\n\n계정 보안을 위해 인증 코드를 타인과 공유하지 마세요. 저희는 고객의 개인 정보를 중요하게 여기며, 인증 과정을 통해 안전한 서비스를 제공하고 있습니다.\n\n궁금한 사항이 있으시면 언제든지 문의해주세요.\n\n감사합니다.";
-            helper.setText(text, true);
-
-            // 이미지 첨부
-            ClassPathResource resource = new ClassPathResource("resources/mail_img.png");
-            helper.addInline("imageId", resource);
-
-            mailSender.send(message);
-        } catch (Exception e) {
-            // 예외 처리
-        }
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(userEntity.getUserId()); // To: 수신자
+        message.setSubject("이때 모해? 이메일 계정 인증"); // Subject 제목
+        message.setText("이때 모해? 계정 인증을 위해 아래의 인증 코드를 사용해주세요:\n\n인증 코드: " +userEntity.getEmailAuthCode()+" \n\n 계정 보안을 위해 인증 코드를 타인과 공유하지 마세요. 저희는 고객의 개인 정보를 중요하게 여기며, 인증 과정을 통해 안전한 서비스를 제공하고 있습니다.\n" +
+                "\n" +
+                "궁금한 사항이 있으시면 언제든지 문의해주세요.\n" +
+                "\n" +
+                "감사합니다.");
+        mailSender.send(message);
     }
+
 
 
     public Boolean verifyEmailCode(UserEntity userEntity, String userVerificationCode) {
